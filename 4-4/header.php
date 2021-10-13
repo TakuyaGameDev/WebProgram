0e8d03e8be3850f5ba0502f553d3845983ced6ad
Takuya Hara <takuya-uverokrock@outlook.jp>
2021-10-13 19:54:39 +0900

    add

---
+++ 4-4/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Lesson Sample Site</title>
+</head>
+<body>
+    <section class = "Header">
+        <div id = "headerBox">
+            <a href = "">新型コロナウイルスに対する取り組みの最新情報をご案内</a>
+        </div>
+        <div id = "backgroundBox">
+        <img id = "eyecatchImg" src = "cafe/img/eyecatch.jpg">
+            <div id = "itemBox">
+                <img id = "logoImg" src = "cafe/img/logo.png" width="247px" height="46.8px">
+                <div id = "menuBox">
+                    <ul>
+                        <li><a href = "">はじめに</a></li>
+                        <li><a href = "">体験</a></li>
+                        <li><a href = "">お問い合わせ</a></li>
+                    </ul>
+                </div>
+                <div id = "signin">サインイン</div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>

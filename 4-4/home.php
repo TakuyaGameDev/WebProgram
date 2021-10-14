f4db3bcc0579af10871edd94d89b44b4b4be01a1
HaraTakuya <takuya-uverokrock@outlook.jp>
2021-10-14 22:38:11 +0900

    1014 night

--- 4-4/home.php
+++ 4-4/home.php
@@ -1,5 +1,26 @@
 <?php include 'header.php';?>
 
 <body>
-
+    <div id = "photoItems">
+        <div id = "tokyo">
+            <span style="background-image: url('cafe/img/cafe1.jpg')"></span>
+            <strong>東京<br>車で15分</strong>
+        </div>
+        <div id = "tokyo">
+            <span style="background-image: url('cafe/img/cafe1.jpg')"></span>
+            <strong>東京<br>車で15分</strong>
+        </div>
+        <div id = "tokyo">
+            <span style="background-image: url('cafe/img/cafe1.jpg')"></span>
+            <strong>東京<br>車で15分</strong>
+        </div>
+        <div id = "tokyo">
+            <span style="background-image: url('cafe/img/cafe1.jpg')"></span>
+            <strong>東京<br>車で15分</strong>
+        </div>
+        <div id = "tokyo">
+            <span style="background-image: url('cafe/img/cafe1.jpg')"></span>
+            <strong>東京<br>車で15分</strong>
+        </div>
+    </div>
 </body>

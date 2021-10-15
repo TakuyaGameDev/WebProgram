2738bd930af45ed04d7dba98d8fd2e0a46338734
Takuya Hara <takuya-uverokrock@outlook.jp>
2021-10-15 20:00:40 +0900

    10/15commit

---
+++ 4-4/footer.php
@@ -0,0 +1,38 @@
+
+<body>
+    <section class = "footerBox">
+        <div id = "parent">
+            <div id = "area">
+            <strong>企業情報</strong>
+            <p>ご利用方法</p>
+            <p>ニュースルーム</p>
+            <p>株主・投資家のみなさまへ</p>
+            <p>xxxxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxxxx</p>
+            <p>採用情報</p>
+            </div>
+            <div id = "area">
+            <strong>コミュニティ</strong>
+            <p>ダイバーシティ</p>
+            <p>アクセシビリティ対応</p>
+            <p>お友達を招待</p>
+            <p>xxxxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxxxx</p>
+            </div>
+            <div id = "area">
+            <strong>ホスト</strong>
+            <p>xxxxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxxxx</p>
+            </div>
+            <div id = "area">
+            <strong>サポート</strong>
+            <p>新型コロナウイルスに対する取り組み</p>
+            <p>ヘルプセンター</p>
+            <p>キャンセルオプション</p>
+            <p>コミュニティサポート</p>
+            <p>信頼＆安全</p>
+            </div>
+        </div>
+    </section>
+</body>
--- 4-4/home.php
+++ 4-4/home.php
@@ -1,26 +1,151 @@
 <?php include 'header.php';?>
 
 <body>
-    <div id = "photoItems">
-        <div id = "tokyo">
-            <span style="background-image: url('cafe/img/cafe1.jpg')"></span>
-            <strong>東京<br>車で15分</strong>
+    <div class = "locationPhotos">
+        <div id = "box">
+            <div id = "photo">
+                <img src = "cafe/img/cafe1.jpg">
+            </div>
+            <div id = "strArea">
+                <strong>東京<br>車で15分</strong>
+            </div>
         </div>
-        <div id = "tokyo">
-            <span style="background-image: url('cafe/img/cafe1.jpg')"></span>
-            <strong>東京<br>車で15分</strong>
+        <div id = "box">
+            <div id = "photo">
+                <img src = "cafe/img/cafe2.jpg">
+            </div>
+            <div id = "strArea">
+                <strong>神奈川<br>車で30分</strong>
+            </div>
         </div>
-        <div id = "tokyo">
-            <span style="background-image: url('cafe/img/cafe1.jpg')"></span>
-            <strong>東京<br>車で15分</strong>
+        <div id = "box">
+            <div id = "photo">
+                <img src = "cafe/img/cafe3.jpg">
+            </div>
+            <div id = "strArea">
+                <strong>愛知<br>車で1時間</strong>
+            </div>
         </div>
-        <div id = "tokyo">
-            <span style="background-image: url('cafe/img/cafe1.jpg')"></span>
-            <strong>東京<br>車で15分</strong>
+        <div id = "box">
+            <div id = "photo">
+                <img src = "cafe/img/cafe4.jpg">
+            </div>
+            <div id = "strArea">
+                <strong>京都<br>車で40分</strong>
+            </div>
         </div>
-        <div id = "tokyo">
-            <span style="background-image: url('cafe/img/cafe1.jpg')"></span>
-            <strong>東京<br>車で15分</strong>
+        <div id = "box">
+            <div id = "photo">
+                <img src = "cafe/img/cafe5.jpg">
+            </div>
+            <div id = "strArea">
+                <strong>岡山<br>車で1.5時間</strong>
+            </div>
+        </div>
+        <div id = "box">
+            <div id = "photo">
+                <img src = "cafe/img/cafe6.jpg">
+            </div>
+            <div id = "strArea">
+                <strong>鹿児島<br>車で50分</strong>
+            </div>
+        </div>
+        <div id = "box">
+            <div id = "photo">
+                <img src = "cafe/img/cafe7.jpg">
+            </div>
+            <div id = "strArea">
+                <strong>沖縄<br>車で2時間</strong>
+            </div>
         </div>
     </div>
+    <div class = "favLocation">
+        <h1>好きなロケーションを選ぼう</h1>
+        <div id = "favLocPhotos">
+            <div id = "area">
+                <img src = "cafe/img/intro1.jpg">
+                <div id = "strArea">
+                    <strong>クラシック</strong>
+                </div>
+            </div>
+            <div id = "area">
+                    <img src = "cafe/img/intro2.jpg">
+                <div id = "strArea">
+                    <strong>バー</strong>
+                </div>
+            </div>
+            <div id = "area">
+                    <img src = "cafe/img/intro3.jpg">
+                <div id = "strArea">
+                    <strong>キャンプ</strong>
+                </div>
+            </div>
+            <div id = "area">
+                    <img src = "cafe/img/intro4.jpg">
+                <div id = "strArea">
+                    <strong>リゾート</strong>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class = "GoToEats">
+        <div id = "filter"></div>
+            <div id = "strBox">
+                <h1>Go To Eats</h1>
+                <p>キャンペーンを利用して、全国で食事しよう。<br>いつもと違う景色に囲まれてカラダもココロもリフレッシュ。</p>
+            </div>
+        </div>
+    </div>
+    <section class = "Experience">
+        <h1 id = "title">カフェづくりを体験しよう</h1>
+        <strong>お店のエキスパートが案内するユニークな体験（直接対面型またはオンライン）。</strong>
+        <div id = "photoArea">
+            <div id = "area">
+                <img src="cafe/img/exp1.jpg">
+                <div id = "strArea">
+                    <h1>ジョブ体験</h1>
+                    <p>カフェカウンターを体験しよう</p>
+                </div>
+            </div>
+            <div id = "area">
+                <img src="cafe/img/exp2.jpg">
+                <div id = "strArea">
+                    <h1>レシピ体験</h1>
+                    <p>おいしいレシピを考えてみよう</p>
+                </div>
+            </div>
+            <div id = "area">
+                <img src="cafe/img/exp3.jpg">
+                <div id = "strArea">
+                    <h1>プロモーション体験</h1>
+                    <p>お店の宣伝を手伝ってみよう</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class = "joinHost">
+        <h1 id = "title">全国のホストに仲間入りしよう。</h1>
+        <div id = "photoArea">
+            <div id = "area">
+                <img src ="cafe/img/host1.jpg">
+                <div id = "strArea">
+                    <h1>ビジネス</h1>
+                </div>
+            </div>
+            <div id = "area">
+                <img src ="cafe/img/host2.jpg">
+                <div id = "strArea">
+                    <h1>コミュニティ</h1>
+                </div>
+            </div>
+            <div id = "area">
+                <img src ="cafe/img/host3.jpg">
+                <div id = "strArea">
+                    <h1>食べ歩き</h1>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
+
+<?php include 'footer.php';?>
